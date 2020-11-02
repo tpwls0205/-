fd66f1f33c697af38d91232eb38103b6b4da10ae
se_jin <jangsj4122@gmail.com>
2020-11-02 15:39:45 +0900

    no message

---
+++ C#언어/C#/14_GenericWhere/14_GenericWhere/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _14_GenericWhere
+{
+    class Wrapper<T> where T : struct  // 클래스(힙):래퍼런스 , 스트럭트(스택):밸류
+    {                                   // where를 통해 제약조건을 건다.
+        T Value;
+        public Wrapper() { Value = default(T); }
+        public Wrapper(T aValue)
+        { Value = aValue; }
+        public T Data
+        {
+            get { return Value; }
+            set { Value = value; }
+        }
+        public void OutValue()
+        {
+            Console.WriteLine(Value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wrapper<int> gi =
+                new Wrapper<int>(1234);
+            gi.OutValue();
+            Wrapper<float> gf =
+            new Wrapper<float>(3.14f);
+            gf.OutValue();
+            Wrapper<string> gs =
+            new Wrapper<string>("대한민국");
+            gs.OutValue();
+        }
+    }
+}
---
+++ C#언어/C#/15_GenericList/15_GenericList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Collections : 자료구조
+namespace _15_GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 5; i++)
+                list.Add(i);
+
+            foreach (int element in list)
+                Console.Write("{0} ", element);
+
+            Console.WriteLine();
+            list.RemoveAt(2);
+            foreach (int element in list)
+                Console.Write("{0} ", element);
+        }
+    }
+}
