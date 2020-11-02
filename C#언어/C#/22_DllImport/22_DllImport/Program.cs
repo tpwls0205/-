9a1f9a14bfd4994b094ddbbcba3885910819ccc7
se_jin <jangsj4122@gmail.com>
2020-11-02 15:40:23 +0900

    no message

---
+++ C#언어/C#/22_DllImport/22_DllImport/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;   // C, C++ 함수를 직접 호출
+
+namespace _22_DllImport     // 보안과 같은 일을 할때 -> pinvoke.net -> 함수원형을 찾아 선언하고 사용하면 된다
+{
+    class Program
+    {
+        #region 외부 DLL 정의문
+        [DllImport("User32.dll")]
+        public static extern int MessageBox(int hParent,
+            string Message, string Caption, int Type);
+        //int WINAPI MessageBox(HWND hWnd,
+        //  LPCTSTR lpText, LPCTSTR lpCaption, UINT uType
+        //);
+        [DllImport("Kernel32.dll")]
+         public static extern uint WinExec(string Path,
+            uint nCmdShow);       
+        //UINT WINAPI WinExec(LPCSTR lpCmdLine,
+        //            UINT   uCmdShow);
+
+        #endregion
+        static void Main(string[] args)
+        {
+            //MessageBox(NULL, "", "", MB_OK);
+            MessageBox(0, "메모장을 실행합니다.",
+                "알림", 0); 
+            //WinExec("notepad.exe", SW_SHOW);
+            WinExec("notepad.exe", 1);
+        }
+    }
+}
---
+++ C#언어/C#/26_Chogija/26_Chogija/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _26_Chogija
+{
+    class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Jang = new Human              // ()로 하는건 생성자 함수를 만들고 값을 줘야하는데
+                                                // {}는 프로퍼티를 만들어 두면 안에 초기값을 줄 수 있다.
+                                                // 생성자는 객체생성할 때 만들어지고
+                                                // 초기자는 객체생성을 한 후 값을 준다
+                    { Name = "장길산", Age = 32 };
+            Console.WriteLine("이름 : " + Jang.Name
+                        + ", 나이 : " + Jang.Age);
+        }
+    }
+}
