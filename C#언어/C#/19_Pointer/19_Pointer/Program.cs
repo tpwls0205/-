7e72bc496387b8741a20755c747027e365c6e86a
se_jin <jangsj4122@gmail.com>
2020-11-02 15:40:04 +0900

    no message

---
+++ C#언어/C#/19_Pointer/19_Pointer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _19_Pointer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3, j;
+            // C# 포인터를 사용하려면 : 프로젝트 -> 속성 -> 빌드 -> 안전하지 않은 코드 허용 체크
+            unsafe  // C# 포인터는 unsafe라는 범위 내에서 사용
+            {
+                int* pi;
+                pi = &i;
+                j = *pi;
+            }
+            Console.WriteLine("i={0}, j={1}", i, j);
+        }
+    }
+}
---
+++ C#언어/C#/20_ClassPointer/20_ClassPointer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20_ClassPointer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 1, 2, 3, 4, 5 };   // 배열은 클래스(힙메모리, 래퍼런스)
+            unsafe
+            {
+                fixed (int* pi = &ar[0])    // 계좌번호, 금고의 위치를 고정시킨다(CLR의 임의적으로 데이터 위치를 옮기지 못하도록)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine(pi[i]);
+                    }
+                }
+            }
+        }
+    }
+}
